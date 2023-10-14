9cc79e2e83179db2a192c7f6c2f242191974a69a
Miguel Leon <miguel-leon@aj.com.py>
2023-10-14 11:40:49 -0300

    Correccion dba

--- bd respaldo/helpdesk.sql
+++ bd respaldo/helpdesk.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: sql113.infinityfree.com
--- Tiempo de generación: 10-10-2023 a las 09:11:31
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 7.2.22
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2023 a las 16:40:10
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -51,16 +50,16 @@ CREATE TABLE `t_asignacion` (
 --
 
 INSERT INTO `t_asignacion` (`id_asignacion`, `id_oficina`, `id_equipo`, `nombreEquipoA`, `rotulado`, `marca`, `modelo`, `numeroSerie`, `descripcion`, `memoria`, `tipo_ram`, `disco_duro`, `procesador`, `sistema_operativo`, `fecha_insert`) VALUES
-(4, 3, 2, '', NULL, 'HP', 'gamer', 'verde', 'Aca va la descripción', '32gb', NULL, '1tb', 'i9', NULL, '2023-10-10 09:09:51'),
-(7, 5, 9, '', NULL, 'EPSON', 'PRO', 'Blanco', 'Impresora tipo fotocopiadora ', '', NULL, '', '', NULL, '2023-10-10 09:09:51'),
-(10, 1, 2, '', NULL, '1', 'GT547', NULL, 'descrip', 'RAM', NULL, 'DIS', 'PRO', NULL, '2023-10-10 09:09:51'),
-(12, 5, 1, 'nombre equipo', NULL, 'Marca', 'Modelo', 'tag', 'Descripción', 'memoria', NULL, 'disco duro', 'procesador ', NULL, '2023-10-10 09:09:51'),
-(13, 3, 1, 'APE2SE001', NULL, 'HP', '250g7', '4A185048W', 'Pc nueva', '8GB', NULL, '250GB', 'i3', NULL, '2023-10-10 09:09:51'),
-(15, 8, 1, 'ETPC001', NULL, 'HP', 'HP All-in-One ', '4A185048X', '', '8GB', NULL, '500 GB', 'i3', NULL, '2023-10-10 09:09:51'),
-(16, 7, 2, 'MDEPC001', NULL, 'DELL', 'AX800', 'AJRE1000', 'PC negro nuevo con mouse, teclado y un parlante ', '8GB', NULL, '250 gb', 'i3 8th', NULL, '2023-10-10 09:09:51'),
-(18, 3, 9, 'APE2SE006', '', 'Marca', 'GT547', '4A185048W', '', '', '', '', '', 'Windows 10 32 bits', '2023-10-10 09:09:51'),
-(19, 3, 6, 'nombre equipo', 'ram', 'marca', 'modelo', 'tag', '', '', 'ram', '', '', 'Windows 10 32 bits', '2023-10-10 09:09:51'),
-(20, 3, 2, 'nombre equipo', 'DD4', 'CANNON', 'GT547', 'tag', '', '3GB', 'DD4', 'disco duro', 'i3', 'Windows 7 64 bits', '2023-10-10 09:09:51');
+(4, 3, 2, '', NULL, 'HP', 'gamer', 'verde', 'Aca va la descripción', '32gb', NULL, '1tb', 'i9', NULL, '2023-10-14 09:16:53'),
+(7, 5, 9, '', NULL, 'EPSON', 'PRO', 'Blanco', 'Impresora tipo fotocopiadora ', '', NULL, '', '', NULL, '2023-10-14 09:16:53'),
+(10, 1, 2, '', NULL, '1', 'GT547', NULL, 'descrip', 'RAM', NULL, 'DIS', 'PRO', NULL, '2023-10-14 09:16:53'),
+(12, 5, 1, 'nombre equipo', NULL, 'Marca', 'Modelo', 'tag', 'Descripción', 'memoria', NULL, 'disco duro', 'procesador ', NULL, '2023-10-14 09:16:53'),
+(13, 3, 1, 'APE2SE001', NULL, 'HP', '250g7', '4A185048W', 'Pc nueva', '8GB', NULL, '250GB', 'i3', NULL, '2023-10-14 09:16:53'),
+(15, 8, 1, 'ETPC001', NULL, 'HP', 'HP All-in-One ', '4A185048X', '', '8GB', NULL, '500 GB', 'i3', NULL, '2023-10-14 09:16:53'),
+(16, 7, 2, 'MDEPC001', NULL, 'DELL', 'AX800', 'AJRE1000', 'PC negro nuevo con mouse, teclado y un parlante ', '8GB', NULL, '250 gb', 'i3 8th', NULL, '2023-10-14 09:16:53'),
+(18, 3, 9, 'APE2SE006', '', 'Marca', 'GT547', '4A185048W', '', '', '', '', '', 'Windows 10 32 bits', '2023-10-14 09:16:53'),
+(19, 3, 6, 'nombre equipo', 'ram', 'marca', 'modelo', 'tag', '', '', 'ram', '', '', 'Windows 10 32 bits', '2023-10-14 09:16:53'),
+(20, 3, 2, 'nombre equipo', 'DD4', 'CANNON', 'GT547', 'tag', '', '3GB', 'DD4', 'disco duro', 'i3', 'Windows 7 64 bits', '2023-10-14 09:16:53');
 
 -- --------------------------------------------------------
 
@@ -80,11 +79,11 @@ CREATE TABLE `t_cat_equipos` (
 --
 
 INSERT INTO `t_cat_equipos` (`id_equipo`, `nombre`, `descripcion`, `fecha_insert`) VALUES
-(1, 'PC', 'fas fa-desktop', '2023-10-10 09:09:27'),
-(2, 'Laptop', 'fas fa-laptop', '2023-10-10 09:09:27'),
-(5, 'Monitor', 'fas fa-desktop', '2023-10-10 09:09:27'),
-(6, 'Fotocopiadora', 'fas fa-print', '2023-10-10 09:09:27'),
-(9, 'Impresora', 'fas fa-print', '2023-10-10 09:09:27');
+(1, 'PC', 'fas fa-desktop', '2023-10-14 09:17:11'),
+(2, 'Laptop', 'fas fa-laptop', '2023-10-14 09:17:11'),
+(5, 'Monitor', 'fas fa-desktop', '2023-10-14 09:17:11'),
+(6, 'Fotocopiadora', 'fas fa-print', '2023-10-14 09:17:11'),
+(9, 'Impresora', 'fas fa-print', '2023-10-14 09:17:11');
 
 -- --------------------------------------------------------
 
@@ -96,6 +95,7 @@ CREATE TABLE `t_cat_roles` (
   `id_rol` int(11) NOT NULL,
   `nombre` varchar(245) NOT NULL,
   `descripcion` varchar(245) DEFAULT NULL,
+  `prioridad` int(11) NOT NULL,
   `fecha_insert` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -103,9 +103,9 @@ CREATE TABLE `t_cat_roles` (
 -- Volcado de datos para la tabla `t_cat_roles`
 --
 
-INSERT INTO `t_cat_roles` (`id_rol`, `nombre`, `descripcion`, `fecha_insert`) VALUES
-(1, 'cliente', 'Es un cliente', '2022-01-03 11:44:18'),
-(2, 'admin', 'Es el Administrador del sistema', '2022-01-03 11:44:18');
+INSERT INTO `t_cat_roles` (`id_rol`, `nombre`, `descripcion`, `prioridad`, `fecha_insert`) VALUES
+(1, 'cliente', 'Es un cliente', 1, '2022-01-03 11:44:18'),
+(2, 'admin', 'Es el Administrador del sistema', 10, '2022-01-03 11:44:18');
 
 -- --------------------------------------------------------
 
@@ -126,8 +126,8 @@ CREATE TABLE `t_oficina` (
 --
 
 INSERT INTO `t_oficina` (`id_oficina`, `nombre`, `telefono`, `correo`, `fecha_insert`) VALUES
-(1, 'Informática', '0981288060', 'marferantonio@gmail.com', '2022-01-03 08:47:00'),
-(3, 'apelación', '12454', 'apel@gmail.com', '2022-01-03 09:12:43'),
+(1, 'InformÃÂ¡tica', '0981288060', 'marferantonio@gmail.com', '2022-01-03 08:47:00'),
+(3, 'apelaciÃ³n', '12454', 'apel@gmail.com', '2022-01-03 09:12:43'),
 (4, 'Mesa de Entrada', '11111111', 'MESA@GMAIL.COM', '2022-01-03 09:19:17'),
 (5, 'telecentro', '0981288060', 'telecentrocsj@gmail.com', '2022-01-04 10:49:36'),
 (7, 'Mesa de Entrada', '4545445', 'MESA@GMAIL.COM', '2022-02-03 08:05:33'),
@@ -173,11 +173,11 @@ CREATE TABLE `t_recepcion` (
 --
 
 INSERT INTO `t_recepcion` (`id_recepcion`, `id_equipo`, `nombre_equipo`, `fecha_recepcion`, `rotulado`, `numero_serie`, `ciudad`, `procedencia`, `descripcion_problema`, `recibido`, `responsable`, `descripcion_solucion`, `fecha_entrega`, `estatus`, `nombre_tecnico`, `informe_tecnico`, `fecha_insert`) VALUES
-(1, 1, 'PCMES21', '2022-03-15 12:25:25', '1998-2222-229', '4A185048W', 'San Lorenzo', 'Procedencia', 'Problema', 'Recibido', 'Responsable', 'x', 'Fecha de Entrega', 2, 'Marcos Fernández', 'Informe', '2023-10-10 09:10:29'),
-(3, 1, 'PCMES21', '2022-03-15 13:59:58', '1998-2222-229', '4A185048W', 'San Lorenzo', 'RECURSOS HUMANOS', 'error 404', 'Gente', 'Responsable', 'X', 'Fecha de Entrega', 2, 'Marcos Fernández', 'a', '2023-10-10 09:10:29'),
-(6, 9, 'IMPRE338', '2022-03-17 09:30:10', 'IMP-2222-229', '4A185048IMP', 'San Lorenzo', 'TRIBUNAL DE LA NIÑEZ Y ADOLESCENCIA', 'No enciende', 'Leonardo Villasanti', 'a', 'x', '16.04.22', 0, '', '', '2023-10-10 09:10:29'),
-(8, 1, 'nombre equipo', '2022-03-17 10:33:34', 'rotulado', 'tag', 'San Lorenzo', 'INFORMATICA', 'no', 'Marcos Fernández', 'Responsable', 'solucionado ', '16.03.22', 0, '', '', '2023-10-10 09:10:29'),
-(16, 1, 'nombre equipo', '2022-03-29 08:18:29', '1998-2222-229', '4A185048W', 'San Lorenzo', 'TRIBUNAL DE APELACION CIVIL, COMERCIAL Y LABORAL SEGUNDA SALA', 'x', 'Ernesto Barrios', 'Responsable', 'X', 'Fecha de Entrega', 2, 'Marcos Fernández', '', '2023-10-10 09:10:29');
+(1, 1, 'PCMES21', '2022-03-15 12:25:25', '1998-2222-229', '4A185048W', 'San Lorenzo', 'Procedencia', 'Problema', 'Recibido', 'Responsable', 'x', 'Fecha de Entrega', 2, 'Marcos Fernández', 'Informe', '2023-10-14 09:17:35'),
+(3, 1, 'PCMES21', '2022-03-15 13:59:58', '1998-2222-229', '4A185048W', 'San Lorenzo', 'RECURSOS HUMANOS', 'error 404', 'Gente', 'Responsable', 'X', 'Fecha de Entrega', 2, 'Marcos Fernández', 'a', '2023-10-14 09:17:35'),
+(6, 9, 'IMPRE338', '2022-03-17 09:30:10', 'IMP-2222-229', '4A185048IMP', 'San Lorenzo', 'TRIBUNAL DE LA NIÑEZ Y ADOLESCENCIA', 'No enciende', 'Leonardo Villasanti', 'a', 'x', '16.04.22', 0, '', '', '2023-10-14 09:17:35'),
+(8, 1, 'nombre equipo', '2022-03-17 10:33:34', 'rotulado', 'tag', 'San Lorenzo', 'INFORMATICA', 'no', 'Marcos Fernández', 'Responsable', 'solucionado ', '16.03.22', 0, '', '', '2023-10-14 09:17:35'),
+(16, 1, 'nombre equipo', '2022-03-29 08:18:29', '1998-2222-229', '4A185048W', 'San Lorenzo', 'TRIBUNAL DE APELACION CIVIL, COMERCIAL Y LABORAL SEGUNDA SALA', 'x', 'Ernesto Barrios', 'Responsable', 'X', 'Fecha de Entrega', 2, 'Marcos Fernández', '', '2023-10-14 09:17:35');
 
 -- --------------------------------------------------------
 
@@ -203,8 +203,29 @@ CREATE TABLE `t_reportes` (
 --
 
 INSERT INTO `t_reportes` (`id_reporte`, `id_usuario`, `id_equipo`, `id_usuario_tecnico`, `usuario_tecnico`, `descripcion_problema`, `solucion_problema`, `estatus`, `fecha`, `fecha_insert`) VALUES
-(40, 5, 2, 1, '', 'tengo una falla en mi office...', 'estamos trabajando en la solución', 1, '2022-04-04 09:01:01', '2023-10-10 09:10:50'),
-(41, 5, 2, 1, 'Elsidia Caballero', 'x', 'solucionado', 0, '2022-04-04 09:03:45', '2023-10-10 09:10:50');
+(40, 5, 2, 1, '', 'tengo una falla en mi office...', 'estamos trabajando en la solución', 1, '2022-04-04 09:01:01', '2023-10-14 09:17:51'),
+(41, 5, 2, 1, 'Elsidia Caballero', 'x', 'solucionado', 0, '2022-04-04 09:03:45', '2023-10-14 09:17:51');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_sucursales`
+--
+
+CREATE TABLE `t_sucursales` (
+  `id_sucursal` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `direccion` varchar(500) NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT 1,
+  `fecha_insert` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_sucursales`
+--
+
+INSERT INTO `t_sucursales` (`id_sucursal`, `descripcion`, `direccion`, `estado`, `fecha_insert`) VALUES
+(1, 'Sajonia Y lideressfd', 'Enrique Segoviano', 1, '2023-10-14 10:09:33');
 
 -- --------------------------------------------------------
 
@@ -220,18 +241,19 @@ CREATE TABLE `t_usuarios` (
   `password` varchar(245) NOT NULL,
   `ubicacion` text DEFAULT NULL,
   `activo` int(11) NOT NULL DEFAULT 1,
-  `fecha_insert` datetime DEFAULT current_timestamp()
+  `fecha_insert` datetime DEFAULT current_timestamp(),
+  `id_sucursal` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `t_usuarios`
 --
 
-INSERT INTO `t_usuarios` (`id_usuario`, `id_rol`, `id_oficina`, `usuario`, `password`, `ubicacion`, `activo`, `fecha_insert`) VALUES
-(1, 2, 1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Primer piso', 1, '2022-01-03 08:57:33'),
-(3, 2, 3, 'Rocio ', '011c945f30ce2cbafc452f39840f025693339c42', 'Tercer Piso', 1, '2022-01-03 09:19:17'),
-(5, 1, 7, 'MDE001', '6e6484c552ab654fe83a243bdf159a35bcd81dcb', 'Planta baja', 1, '2022-02-03 08:05:33'),
-(6, 1, 8, 'ESTA001', 'e8040662711125241cdab6e1da4f6b24587dd8a5', 'Planta baja', 1, '2022-02-03 08:07:45');
+INSERT INTO `t_usuarios` (`id_usuario`, `id_rol`, `id_oficina`, `usuario`, `password`, `ubicacion`, `activo`, `fecha_insert`, `id_sucursal`) VALUES
+(1, 2, 1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Primer piso', 1, '2022-01-03 08:57:33', 1),
+(3, 2, 3, 'Rocio ', '011c945f30ce2cbafc452f39840f025693339c42', 'Tercer Piso', 1, '2022-01-03 09:19:17', 1),
+(5, 1, 7, 'MDE001', '6e6484c552ab654fe83a243bdf159a35bcd81dcb', 'Planta baja', 1, '2022-02-03 08:05:33', 1),
+(6, 1, 8, 'ESTA001', 'e8040662711125241cdab6e1da4f6b24587dd8a5', 'Planta baja', 1, '2022-02-03 08:07:45', 1);
 
 --
 -- Índices para tablas volcadas
@@ -279,12 +301,21 @@ ALTER TABLE `t_reportes`
   ADD KEY `id_equipo` (`id_equipo`);
 
 --
+-- Indices de la tabla `t_sucursales`
+--
+ALTER TABLE `t_sucursales`
+  ADD PRIMARY KEY (`id_sucursal`),
+  ADD UNIQUE KEY `descripcion` (`descripcion`);
+
+--
 -- Indices de la tabla `t_usuarios`
 --
 ALTER TABLE `t_usuarios`
   ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
   ADD KEY `fkoficina_idx` (`id_oficina`),
-  ADD KEY `fkRoles_idx` (`id_rol`);
+  ADD KEY `fkRoles_idx` (`id_rol`),
+  ADD KEY `fk_usuarios_sucursales` (`id_sucursal`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -327,6 +358,12 @@ ALTER TABLE `t_reportes`
   MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
+-- AUTO_INCREMENT de la tabla `t_sucursales`
+--
+ALTER TABLE `t_sucursales`
+  MODIFY `id_sucursal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de la tabla `t_usuarios`
 --
 ALTER TABLE `t_usuarios`
@@ -342,6 +379,12 @@ ALTER TABLE `t_usuarios`
 ALTER TABLE `t_asignacion`
   ADD CONSTRAINT `t_asignacion_ibfk_1` FOREIGN KEY (`id_oficina`) REFERENCES `t_oficina` (`id_oficina`),
   ADD CONSTRAINT `t_asignacion_ibfk_2` FOREIGN KEY (`id_equipo`) REFERENCES `t_cat_equipos` (`id_equipo`);
+
+--
+-- Filtros para la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  ADD CONSTRAINT `fk_usuarios_sucursales` FOREIGN KEY (`id_sucursal`) REFERENCES `t_sucursales` (`id_sucursal`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
