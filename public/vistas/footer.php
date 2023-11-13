c27d29f6f1df392d809deaaac03b4fce19674f2d
Mitxel Code <yosoymitxel@gmail.com>
2023-11-13 13:16:11 -0500

    Header y Footer

--- public/vistas/footer.php
+++ public/vistas/footer.php
@@ -1,45 +1,45 @@
-<div class="d-block" style="margin-top: 200px">
+        <div class="d-block" style="margin-top: 200px">
 
-</div>
-<footer id="footer" class="bg-dark" style="left: 0;bottom: 0;overflow: hidden;position: absolute;width: 100%;">
-    <div class="container my-3">
-        <div class="row justify-content-center" style="">
-            <div class="col-12 ">
-                <a class="mx-auto  navbar-brand" href="inicio.php" style="justify-content: center;display: flex;">
-                    <img src="../img//logoicono.ico" width="" style="height: 50px;">
-                </a>
-            </div>
-            <hr>
         </div>
-        <div class="row">
-            <div class="col text-center text-white">
-                <p>© Copiright 2023 | Marcos Fernández</p>
-            </div>
-            <hr>
-        </div>	
-    </div>	
-</footer>
-<script src="../../public/jquery/jquery-3.6.0.min.js"></script> 
-<script src="../../public/bootstrap/popper.min.js"></script>
-<script src="../../public/bootstrap/bootstrap.min.js"></script>
-<script src="../../public/datatable/jquery.dataTables.min.js"></script>
-<script src="../../public/datatable/dataTables.bootstrap4.min.js"></script>
-<script src="../datatable/dataTables.responsive.min.js"></script>
-<script src="../datatable/responsive.bootstrap4.min.js"></script>
-<script src="../sweetalert2/sweetalert2@11.js"></script>
-<script src="../../public/js/inicio/main.js"></script>
+        <footer id="footer" class="bg-dark" style="left: 0;bottom: 0;overflow: hidden;position: absolute;width: 100%;">
+            <div class="container my-3">
+                <div class="row justify-content-center" style="">
+                    <div class="col-12 ">
+                        <a class="mx-auto  navbar-brand" href="inicio.php" style="justify-content: center;display: flex;">
+                            <img src="../img//logoicono.ico" width="" style="height: 50px;">
+                        </a>
+                    </div>
+                    <hr>
+                </div>
+                <div class="row">
+                    <div class="col text-center text-white">
+                        <p>© Copiright 2023 | Marcos Fernández</p>
+                    </div>
+                    <hr>
+                </div>	
+            </div>	
+        </footer>
+        <script src="../../public/jquery/jquery-3.6.0.min.js"></script> 
+        <script src="../../public/bootstrap/popper.min.js"></script>
+        <script src="../../public/bootstrap/bootstrap.min.js"></script>
+        <script src="../../public/datatable/jquery.dataTables.min.js"></script>
+        <script src="../../public/datatable/dataTables.bootstrap4.min.js"></script>
+        <script src="../datatable/dataTables.responsive.min.js"></script>
+        <script src="../datatable/responsive.bootstrap4.min.js"></script>
+        <script src="../sweetalert2/sweetalert2@11.js"></script>
+        <script src="../../public/js/inicio/main.js"></script>
 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script id="dev-js-devbrary" src="https://cdn.jsdelivr.net/gh/yosoymitxel/devbrary-js-test-library@master/devbrary.js"></script> 
-<!--SECCOIN BOTONES DATA TABLE-->
-<script src="../../public/datatable/dataTables.buttons.min.js"></script>
-<script src="../../public/datatable/jszip.min.js"></script>
-<script src="../../public/datatable/pdfmake.min.js"></script>
-<script src="../../public/datatable/vfs_fonts.js"></script>
-<script src="../../public/datatable/buttons.html5.min.js"></script>
-<script src="../../public/datatable/jszip.min.js"></script>
-<script src="../../public/datatable/buttons.print.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
-</body>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script id="dev-js-devbrary" src="https://cdn.jsdelivr.net/gh/yosoymitxel/devbrary-js-test-library@master/devbrary.js"></script> 
+        <!--SECCOIN BOTONES DATA TABLE-->
+        <script src="../../public/datatable/dataTables.buttons.min.js"></script>
+        <script src="../../public/datatable/jszip.min.js"></script>
+        <script src="../../public/datatable/pdfmake.min.js"></script>
+        <script src="../../public/datatable/vfs_fonts.js"></script>
+        <script src="../../public/datatable/buttons.html5.min.js"></script>
+        <script src="../../public/datatable/jszip.min.js"></script>
+        <script src="../../public/datatable/buttons.print.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
+    </body>
 </html>
